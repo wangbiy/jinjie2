85541f878928845378dad372e2cb0c4791ac27e3
王碧瑶 <2487036948@qq.com>
2019-05-23 21:29:36 +0800

    2019_5_23

---
+++ test_2019_5_23_5/test_2019_5_23_5/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NOP_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char* p = "abcdef";
+	printf("%d\n", sizeof(p));//4 指针变量大小
+	printf("%d\n", sizeof(p + 1));//4
+	printf("%d\n", sizeof(*p));//1  p存'a'的地址，对p解引用，为'a'的大小
+	printf("%d\n", sizeof(p[0]));//1
+	printf("%d\n", sizeof(&p));//4
+	printf("%d\n", sizeof(&p + 1));//4char* p=&a,char* *q=&p; q+1跳过一个char*  ,&p+1 跳过一个char*
+	printf("%d\n", sizeof(&p[0] + 1));//4 'b'的地址
+	printf("****************\n");
+	printf("%d\n", strlen(p));//6
+	printf("%d\n", strlen(p + 1));//5
+	printf("%d\n", strlen(*p));//error 首元素的地址解引用为首元素
+	printf("%d\n", strlen(p[0]));//error
+	printf("%d\n", strlen(&p));//随机值 整个数组的地址
+	printf("%d\n", strlen(&p + 1));//随机值 跳过一个char*
+	printf("%d\n", strlen(&p[0] + 1));//5
+
+	system("pause");
+	return 0;
+}
