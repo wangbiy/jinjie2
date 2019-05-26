db99ab48b84db7322716ebf910332eec7a06a795
Daisy <49625347+wangbiy@users.noreply.github.com>
2019-05-26 18:23:15 +0800

    Update test.c

--- test_2019_5_26_2/test_2019_5_26_2/test.c
+++ test_2019_5_26_2/test_2019_5_26_2/test.c
@@ -25,7 +25,7 @@ char* my_strncpy(char*dest, const char*src, size_t count)
 int main()
 {
 	//char arr[] = "aaaaaaaaaaaa";
-	//strncpy(arr, "bit", 5);//当超过要拷贝的内容的长度时，自动向后面自动补剩下的0，小于要拷贝的内容的长度时，不补0，结果为bit
+	//strncpy(arr, "bit", 5);//当超过要拷贝的内容的长度时，自动向后面自动补剩下的0，小于要拷贝的内容的长度时，不补0，结果为bitaaaaaaaaa
 	char arr[] = "aaaaaaaaaaaa";
 	my_strncpy(arr, "bit", 3);//bitaaaaaaaaa
 	//my_strncpy(arr, "bit", 5);//bit
