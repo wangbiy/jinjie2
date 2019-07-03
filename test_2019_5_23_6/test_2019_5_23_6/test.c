290ed89c5242f60e8d49002a133513edc9df9462
Daisy <49625347+wangbiy@users.noreply.github.com>
2019-07-03 10:26:33 +0800

    数组笔试题5

--- test_2019_5_23_6/test_2019_5_23_6/test.c
+++ test_2019_5_23_6/test_2019_5_23_6/test.c
@@ -4,18 +4,17 @@
 int main()
 {
 	int a[3][4] = { 0 };
-	printf("%d\n", sizeof(a));//48 ��������Ĵ�С
-	printf("%d\n", sizeof(a[0][0]));//4 ��Ԫ�صĴ�С
-	printf("%d\n", sizeof(a[0]));//16 a[0]��ʾ��һ���������������һ�еĴ�С
-	printf("%d\n", sizeof(a[0]+1));//4 a[0]Ϊ��Ԫ�ص�ַ����ʾ��һ�е�һ��Ԫ�ص�ַ��+1Ϊ��һ�еڶ���Ԫ�ص�ַ
+	printf("%d\n", sizeof(a));//48 整个数组的大小
+	printf("%d\n", sizeof(a[0][0]));//4 首元素的大小
+	printf("%d\n", sizeof(a[0]));//16 a[0]表示第一行数组名，计算第一行的大小
+	printf("%d\n", sizeof(a[0]+1));//4 a[0]为首元素地址，表示第一行第一个元素地址，+1为第一行第二个元素地址
 	printf("%d\n", sizeof(*(a[0]+1)));//4
-	printf("%d\n", sizeof(a+1));// 4 aΪ��һ�е�ַ��+1Ϊ�ڶ��е�ַ
-	printf("%d\n", sizeof(*(a+1)));//16 �ڶ��еĴ�С
-	printf("%d\n", sizeof(&a[0]+1));//4 �ڶ��е�ַ
-	printf("%d\n", sizeof(*(&a[0]+1)));//16 �ڶ��д�С
-	printf("%d\n", sizeof(*a));//16 ��һ�д�С
-	printf("%d\n", sizeof(a[3]));// 16 ĳһ�е���������ĳһ�еĴ�С
-
+	printf("%d\n", sizeof(a+1));// 4 a为第一行地址，+1为第二行地址
+	printf("%d\n", sizeof(*(a+1)));//16 第二行的大小
+	printf("%d\n", sizeof(&a[0]+1));//4 第二行地址
+	printf("%d\n", sizeof(*(&a[0]+1)));//16 第二行大小
+	printf("%d\n", sizeof(*a));//16 第一行大小
+	printf("%d\n", sizeof(a[3]));// 16 某一行的数组名，某一行的大小
 	system("pause");
 	return 0;
 }
